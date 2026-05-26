285c82d7994648d3854a37c48b6b2af6af2a461d
uratsakidogi1488 <tottenkopf20@gmail.com>
2026-05-27 02:14:09 +0300

    Update twrp_WP15.mk

--- twrp_WP15.mk
+++ twrp_WP15.mk
@@ -2,36 +2,25 @@
 # Copyright (C) 2023 The TWRP Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 #
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+# Installs GSI keys into ramdisk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-# Inherit from star device
+# Inherit from device configuration
 $(call inherit-product, device/oukitel/WP15/device.mk)
 
-# Inherit some common recovery stuff.
+# Inherit common TWRP configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Device identifier. This must come after all inclusions
+# Device identifier
 PRODUCT_DEVICE := WP15
 PRODUCT_NAME := twrp_WP15
 PRODUCT_BRAND := OUKITEL
@@ -45,5 +34,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="WP15_RU-user 11 RP1A.200720.011 1653633312 release-keys"
 
 BUILD_FINGERPRINT := OUKITEL/WP15_RU/WP15:11/RP1A.200720.011/1653633312:user/release-keys
-#PLATFORM_BASE_OS = OUKITEL/WP15_RU/WP15:11/RP1A.200720.011/1640680713:user/release-keys
-
