3fc30664e75cff4f624327eee067c2c331c49a81
uratsakidogi1488 <tottenkopf20@gmail.com>
2026-05-26 18:00:20 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,10 +1,7 @@
-# Device path
 DEVICE_PATH := device/oukitel/WP15
 
-# For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
-# Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
@@ -20,11 +17,9 @@ TARGET_USES_64_BIT_BINDER := true
 TARGET_SUPPORTS_64_BIT_APPS := true
 TARGET_IS_64_BIT := true
 
-# Platform
 TARGET_BOARD_PLATFORM := mt6833
 BOARD_USES_MTK_HARDWARE := true
 
-# Kernel
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.force_normal_boot=1
 BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_KERNEL_BASE := 0x40078000
@@ -42,7 +37,6 @@ BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET) --kernel_offset $(BOARD_KERNEL_OFFSET) --second_offset $(BOARD_KERNEL_SECOND_OFFSET) --dtb $(TARGET_PREBUILT_DTB) --header_version $(BOARD_BOOTIMG_HEADER_VERSION) --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PAGESIZE) --dtb_offset $(BOARD_DTB_OFFSET)
 
-# Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x2800000
 BOARD_SUPER_PARTITION_SIZE := 6442450944
 BOARD_MAIN_PARTITION_LIST := system vendor odm
@@ -51,10 +45,11 @@ TARGET_NO_RECOVERY := true
 TW_HAS_NO_RECOVERY_PARTITION := true
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
 
-# AVB
+TW_TARGET_USES_DYNAMIC_PARTITIONS := true
+TW_INCLUDE_FASTBOOTD := true
+
 BOARD_AVB_ENABLE := false
 
-# Crypto & Decryption (FBE)
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_CRYPTO_FBE := true
 TW_INCLUDE_FBE_METADATA_DECRYPT := true
@@ -64,13 +59,9 @@ TW_CRYPTO_FS_TYPE := "f2fs"
 TW_CRYPTO_REAL_BLKDEV := "/dev/block/by-name/userdata"
 TW_CRYPTO_MNT_POINT := "/data"
 
-# Безопасный поиск библиотек (выкашиваем всё, что связано с радио и RIL)
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += $(shell find $(DEVICE_PATH)/recovery/root/vendor/lib64/ -name "*.so" ! -name "*ril*" ! -name "*radio*" ! -name "*protobuf*")
-
-# Забираем из bin/hw ТОЛЬКО файлы дешифровки, игнорируя системный мусор
 TW_RECOVERY_ADDITIONAL_FILES += $(shell find $(DEVICE_PATH)/recovery/root/vendor/bin/hw/ -name "*keymaster*" -o -name "*gatekeeper*")
 
-# TWRP UI & Settings
 TW_THEME := portrait_hdpi
 DEVICE_SCREEN_WIDTH := 720
 DEVICE_SCREEN_HEIGHT := 1600
@@ -83,14 +74,12 @@ TW_INCLUDE_RESETPROP := true
 TW_INCLUDE_REPACKTOOLS := true
 TW_DEFAULT_LANGUAGE := ru
 
-# Storage
 RECOVERY_SDCARD_ON_DATA := true
 TW_INTERNAL_STORAGE_PATH := "/data/media/0"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 
-# Исключаем лишние модули, которые вызывают ошибки зависимостей
 TW_EXCLUDE_AUDIO := true
 TARGET_EXCLUDE_AUDIO := true
 TW_EXCLUDE_RIL := true
