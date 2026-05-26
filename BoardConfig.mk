ffc2478b438f24fd8ec7e7beb5f36a69e1e5ea6f
uratsakidogi1488 <tottenkopf20@gmail.com>
2026-05-27 02:05:10 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,13 +1,14 @@
 #
-# BoardConfig.mk for Oukitel WP15
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 The TWRP Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
 #
 
 DEVICE_PATH := device/oukitel/WP15
 ALLOW_MISSING_DEPENDENCIES := true
 
-# -------------------------
 # Architecture
-# -------------------------
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
@@ -23,15 +24,22 @@ TARGET_USES_64_BIT_BINDER := true
 TARGET_SUPPORTS_64_BIT_APPS := true
 TARGET_IS_64_BIT := true
 
-# -------------------------
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
+
 # Platform
-# -------------------------
 TARGET_BOARD_PLATFORM := mt6833
 BOARD_USES_MTK_HARDWARE := true
 
-# -------------------------
-# Kernel & Boot
-# -------------------------
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := OUKITEL
+TARGET_NO_BOOTLOADER := true
+TARGET_USES_UEFI := true
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := WP15,WP15_RU
+
+# Kernel
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.force_normal_boot=1
 BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_KERNEL_BASE := 0x40078000
@@ -40,83 +48,161 @@ BOARD_KERNEL_SECOND_OFFSET := 0xbff88000
 BOARD_KERNEL_TAGS_OFFSET := 0x07c08000
 BOARD_DTB_OFFSET := 0x07c08000
 BOARD_KERNEL_PAGESIZE := 2048
-
 BOARD_KERNEL_IMAGE_NAME := Image.gz
+
 TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
 
 BOARD_HASH_TYPE := sha1
 BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_FLASH_BLOCK_SIZE := 131072
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET) --kernel_offset $(BOARD_KERNEL_OFFSET) --second_offset $(BOARD_KERNEL_SECOND_OFFSET) --dtb $(TARGET_PREBUILT_DTB) --header_version $(BOARD_BOOTIMG_HEADER_VERSION) --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PAGESIZE) --dtb_offset $(BOARD_DTB_OFFSET)
 
-# -------------------------
-# Partitions
-# -------------------------
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --second_offset $(BOARD_KERNEL_SECOND_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS += --base $(BOARD_KERNEL_BASE)
+BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
+BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
+
+# File systems and partitions
+BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x2800000
-BOARD_SUPER_PARTITION_SIZE := 6442450944
-BOARD_MAIN_PARTITION_LIST := system vendor odm
 
+# 🔥 ВАЖНО: увеличено для твоего файла шифрования
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x4000000   # 64 МБ
+
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
+
+# Recovery
 BOARD_USES_RECOVERY_AS_BOOT := true
+BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_NO_RECOVERY := true
 TW_HAS_NO_RECOVERY_PARTITION := true
+
+TARGET_COPY_OUT_PRODUCT := product
+TARGET_COPY_OUT_VENDOR := vendor
+
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Dynamic Partition
+BOARD_SUPER_PARTITION_SIZE := 6442450944
+BOARD_SUPER_PARTITION_GROUPS := main
+BOARD_MAIN_SIZE := 6438256640
+BOARD_MAIN_PARTITION_LIST := system product vendor odm
+
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
+BOARD_SUPPRESS_SECURE_ERASE := true
 
-TW_TARGET_USES_DYNAMIC_PARTITIONS := true
-TW_INCLUDE_FASTBOOTD := true
-BOARD_AVB_ENABLE := false
+# AVB
+BOARD_AVB_ENABLE := true
+BOARD_AVB_VBMETA_SYSTEM := system product
+BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 1
 
-# -------------------------
-# Crypto / Decryption (FBE)
-# -------------------------
+BOARD_AVB_VBMETA_VENDOR := vendor
+BOARD_AVB_VBMETA_VENDOR_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_VBMETA_VENDOR_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX_LOCATION := 2
+
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 3
+
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
+PLATFORM_SECURITY_PATCH := 2022-05-05
+PLATFORM_VERSION := 11
+
+# Metadata
+BOARD_USES_METADATA_PARTITION := true
+BOARD_ROOT_EXTRA_FOLDERS += metadata
+
+# Crypto
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_CRYPTO_FBE := true
 TW_INCLUDE_FBE_METADATA_DECRYPT := true
 TW_USE_FSCRYPT_POLICY := 2
 TW_PREPARE_DATA_MEDIA_EARLY := true
+
 TW_CRYPTO_FS_TYPE := "f2fs"
 TW_CRYPTO_REAL_BLKDEV := "/dev/block/by-name/userdata"
 TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "rw,seclabel,nosuid,nodev,noatime,noauto_da_alloc,inlinecrypt,resgid=1065,errors=panic,data=ordered"
 
-# Безопасный поиск библиотек (не крашит сборку, если папки нет)
-TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += $(shell find $(DEVICE_PATH)/recovery/root/system/vendor/lib64/ -type f -name "*.so" ! -name "*ril*" ! -name "*radio*" ! -name "*protobuf*" 2>/dev/null || true)
-TW_RECOVERY_ADDITIONAL_FILES += $(shell find $(DEVICE_PATH)/recovery/root/system/vendor/bin/hw/ -type f -name "*keymaster*" -o -name "*gatekeeper*" 2>/dev/null || true)
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libpuresoftkeymasterdevice
 
-# -------------------------
-# Display & UI
-# -------------------------
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_RECOVERY_INITRC := $(DEVICE_PATH)/recovery/root/init.recovery.mt6833.rc
+
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+TARGET_DISABLE_TRIPLE_BUFFERING := false
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
+
+# TWRP UI
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
+TARGET_RECOVERY_LCD_BACKLIGHT_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 100
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TW_USE_TOOLBOX := true
+TW_EXTRA_LANGUAGES := false
+TW_DEFAULT_LANGUAGE := ru
+TW_NO_SCREEN_BLANK := true
+TW_NO_BATT_PERCENT := false
+TARGET_USES_MKE2FS := true
+TW_INCLUDE_NTFS_3G := true
+
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXCLUDE_TWRPAPP := true
+TW_EXCLUDE_APEX := true
+
+TW_INCLUDE_RESETPROP := true
+TW_INCLUDE_REPACKTOOLS := true
+TW_INCLUDE_LIBRESETPROP := true
+
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
+
 TW_THEME := portrait_hdpi
 DEVICE_SCREEN_WIDTH := 720
 DEVICE_SCREEN_HEIGHT := 1600
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TW_MAX_BRIGHTNESS := 255
-TW_DEFAULT_BRIGHTNESS := 100
-TW_DEFAULT_LANGUAGE := ru
 
-# -------------------------
-# TWRP Features
-# -------------------------
-TW_INCLUDE_NTFS_3G := true
-TW_INCLUDE_RESETPROP := true
-TW_INCLUDE_REPACKTOOLS := true
+TW_STATUS_ICONS_ALIGN := center
+TW_CUSTOM_CLOCK_POS := 310
 
-# -------------------------
-# Storage
-# -------------------------
 RECOVERY_SDCARD_ON_DATA := true
+TW_HAS_MTP := true
+TW_NO_USB_STORAGE := false
 TW_INTERNAL_STORAGE_PATH := "/data/media/0"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_MTP_DEVICE := /dev/mtp_usb
+TW_DEFAULT_EXTERNAL_STORAGE := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
 
-# -------------------------
-# Exclusions (Отрезаем лишнее)
-# -------------------------
-TW_EXCLUDE_AUDIO := true
-TARGET_EXCLUDE_AUDIO := true
-TW_EXCLUDE_RIL := true
-TARGET_EXCLUDE_RIL := true
-TW_EXCLUDE_LIBRIL := true
-TARGET_EXCLUDE_LIBRIL := true
+TW_DEVICE_VERSION := OUKITEL WP15_V05_20220527 by  GK
