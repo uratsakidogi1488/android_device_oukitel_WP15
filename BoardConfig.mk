9123816575fbdfbb2fecae794ed460263c67b8df
uratsakidogi1488 <tottenkopf20@gmail.com>
2026-05-28 16:08:26 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -235,4 +235,4 @@ TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_MTP_DEVICE := /dev/mtp_usb
 TW_DEFAULT_EXTERNAL_STORAGE := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
-TW_DEVICE_VERSION := OUKITEL WP15_V05_20220527 by lopestom
+TW_DEVICE_VERSION := OUKITEL WP15 
