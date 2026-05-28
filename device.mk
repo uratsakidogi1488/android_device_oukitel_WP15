311673aa296c7067a2be0632b08321ff74492eef
uratsakidogi1488 <tottenkopf20@gmail.com>
2026-05-28 03:57:16 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,5 +1,4 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
 # Copyright (C) 2023 The TWRP Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,77 +14,36 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := device/oukitel/WP15
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Enable project quotas and casefolding for emulated storage without sdcardfs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Dynamic Partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
+# Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-# Shipping VNDK
-PRODUCT_TARGET_VNDK_VERSION := 30
+# Inherit from star device
+$(call inherit-product, device/oukitel/WP15/device.mk)
 
-# Shipping API level
-PRODUCT_SHIPPING_API_LEVEL := 30
+# Inherit some common recovery stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-PRODUCT_PLATFORM := mt6833
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := WP15
+PRODUCT_NAME := twrp_WP15
+PRODUCT_BRAND := OUKITEL
+PRODUCT_MODEL := WP15
+PRODUCT_MANUFACTURER := wheatek
+PRODUCT_RELEASE_NAME := OUKITEL WP15
 
-# A/B
-ENABLE_VIRTUAL_AB := true
-AB_OTA_UPDATER := true
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=WP15 \
+    PRODUCT_NAME=WP15_RU \
+    PRIVATE_BUILD_DESC="WP15_RU-user 11 RP1A.200720.011 1653633312 release-keys"
 
-AB_OTA_PARTITIONS += \
-    boot \
-    dtbo \
-    lk \
-    preloader \
-    odm \
-    product \
-    system \
-    vbmeta \
-    vbmeta_vendor \
-    vbmeta_system \
-    vendor \
-    vendor_boot
+BUILD_FINGERPRINT := OUKITEL/WP15_RU/WP15:11/RP1A.200720.011/1653633312:user/release-keys
+#PLATFORM_BASE_OS = OUKITEL/WP15_RU/WP15:11/RP1A.200720.011/1640680713:user/release-keys
 
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-mtkimpl.recovery \
-    android.hardware.boot@1.1-mtkimpl \
-    android.hardware.boot@1.1-service
-
-PRODUCT_PACKAGES_DEBUG += \
-    bootctrl \
-    update_engine_client
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
-    
-# Fastbootd stuff
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery \
-    android.hardware.fastboot@1.0-impl-mtk \
-    fastbootd
-
-# health Hal
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
-
-# MTK PlPath Utils
-PRODUCT_PACKAGES += \
-    mtk_plpath_utils.recovery
-
-TW_OVERRIDE_SYSTEM_PROPS := \
-    "ro.build.product;ro.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
